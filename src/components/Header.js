63722140e22264296e283f8018cb4bbb808be354
shampoo Ma <a0017452406@gmail.com>
2021-06-11 16:49:19 +0800

    finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,33 +8,37 @@ import Col from 'react-bootstrap/Col';
 const Header = (props) => {
     const { setTab } = props;
 
-    return (<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        {/* <Nav className="mr-auto"> */}
-        <Row style={{ width: "100%" }}>
-            <Col xs={2}><Navbar.Brand onClick={() => setTab(0)} > Home</Navbar.Brand>
-            </Col>
-            <Col xs={8}>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <div className="d-flex justify-content-center" style={{ width: "100%" }}>
-                        <div>
-                            <Nav.Link onClick={() => setTab(1)}>About</Nav.Link>
-                        </div>
-                        <div>
-                            <Nav.Link onClick={() => setTab(2)}>No Quote</Nav.Link>
-                        </div>
-                        <div>
-                            <Nav.Link onClick={() => setTab(3)}>Number Typing</Nav.Link>
-                        </div>
+    return (<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{ padding: "10px" }}>
+        <Nav style={{width:"100%"}}>
+            <Row className="justify-content-lg-between  " style={{ width: "100%" }}>
+                <Col xs={12} lg={2}>
+                    <div className="d-flex justify-content-between">
+                        <Navbar.Brand onClick={() => setTab(0)} > Home</Navbar.Brand>
+
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     </div>
+                </Col>
+                <Col xs={12} lg={8}>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <div className="d-flex flex-column flex-lg-row justify-content-center" style={{ width: "100%" }}>
+                            <div>
+                                <Nav.Link onClick={() => setTab(1)}>About</Nav.Link>
+                            </div>
+                            <div>
+                                <Nav.Link onClick={() => setTab(2)}>No Quote</Nav.Link>
+                            </div>
+                            <div>
+                                <Nav.Link onClick={() => setTab(3)}>Number Typing</Nav.Link>
+                            </div>
+                        </div>
 
-                </Navbar.Collapse>
-            </Col>
-            <Col xs={2}><h1>hi</h1></Col>
-        </Row>
+                    </Navbar.Collapse>
+                </Col>
+                <Col xs={0} lg={2}></Col>
+            </Row>
 
+        </Nav>
 
-        {/* </Nav> */}
     </Navbar>)
 }
 
