c8e060184a3a925990e81172cb596e8bbdefe667
Long <leonli21999@gmail.com>
2021-06-12 16:27:52 +0800

    just changes in num

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Footer() {
-    return (<div style={{height: "500px", width: "100%", backgroundColor: "#828282", position: "absolute", bottom: "0px"}}>
+    return (<div style={{height: "200px", width: "100%", backgroundColor: "#828282", position: "absolute", bottom: "0px"}}>
         <h1>Footer</h1>
         </div>)
 }
--- src/components/Ｎｕｍｂｅｒ/Number typing.js
+++ src/components/Ｎｕｍｂｅｒ/Number typing.js
@@ -5,45 +5,62 @@ function NumberTyping_displaySampleText(){
 
 
 	//function 1
-	// function genNumbers() {		
-		
-	// console.log(generatedNumber);
-		
-	// }
-	// genNumbers();
+	
 
 
 
-	////////////////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	//html (i.e. print on the Numer-typing page)
-
-
-
-
+	//functions after return 
 	useEffect(() => {	
-		//clean up the div_displayGeneratedNumber
-		document.getElementById("div_displayGeneratedNumber").innerHTML = "";
+	var generatedNumber
+	var decidedDigits;
+	//clean up the div_displayGeneratedNumber
+	document.getElementById("div_displayGeneratedNumber").innerHTML = "";
+	
+	function genAndDisplayNumWhenInitialize (){		
+
 		
 		//gen 11 numbers
-		for (var i = 0; i <= 10; i++) {
+		for (var i = 0; i < 10; i++) {
 		var randomDicimalToDecideDigits = Math.random();
-		var decidedDigits;
-
 		if (randomDicimalToDecideDigits < 0.1){decidedDigits = 100}
 		else if (randomDicimalToDecideDigits >= 0.1 && randomDicimalToDecideDigits < 0.2) {decidedDigits = 1000}
 		else if (randomDicimalToDecideDigits >= 0.2 && randomDicimalToDecideDigits < 0.5) {decidedDigits = 10000}
 		else if (randomDicimalToDecideDigits >= 0.5 && randomDicimalToDecideDigits < 0.8) {decidedDigits = 100000}
-		else {decidedDigits = 1000000}	
+		else {decidedDigits = 1000000}
+		
 		//gen 11 numbers: the gened number
-		var generatedNumber = Math.floor(Math.random() * decidedDigits);
-		////gen 11 numbers: print the number
+		generatedNumber = Math.floor(Math.random() * decidedDigits);
+		//gen 11 numbers: print the number (each number add new number & linebreak)
 		document.getElementById("div_displayGeneratedNumber").innerHTML += generatedNumber + "<br>";
+		}
+		
+	
 
+		function pressEnterGenAndAddOneNumber() {
+			document.body.addEventListener("keydown", function(pressedKey){if(pressedKey.keyCode === 13){ 
+			var randomDicimalToDecideDigits2 = Math.random();
+			
+			//gen a single number
+			if (randomDicimalToDecideDigits2 < 0.1){decidedDigits = 100}
+			else if (randomDicimalToDecideDigits2 >= 0.1 && randomDicimalToDecideDigits2 < 0.2) {decidedDigits = 1000}
+			else if (randomDicimalToDecideDigits2 >= 0.2 && randomDicimalToDecideDigits2 < 0.5) {decidedDigits = 10000}
+			else if (randomDicimalToDecideDigits2 >= 0.5 && randomDicimalToDecideDigits2 < 0.8) {decidedDigits = 100000}
+			else {decidedDigits = 1000000};
+			generatedNumber = Math.floor(Math.random() * decidedDigits);
+
+			//gen a single number: print & add the new gened number
+			document.getElementById("div_displayGeneratedNumber").innerHTML += generatedNumber + "<br>";}})
+			}
+		pressEnterGenAndAddOneNumber();
 	}
-	})
+	genAndDisplayNumWhenInitialize();
+})//useEffect end.	
 
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	//html (i.e. print on the Numer-typing page)
 	return (
 		<div>
 			<div id="div_displayGeneratedNumber"></div>
@@ -56,7 +73,9 @@ function NumberTyping_displaySampleText(){
 		</div>
 		) 
 
-
+	
 }
-export default NumberTyping_displaySampleText
 
+
+
+export default NumberTyping_displaySampleText
