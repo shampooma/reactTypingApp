cbc09f4445b5771e81501b59df644b9ccd555525
johntheactualman <johnchanabcd123@gmail.com>
2021-06-27 15:41:00 +0800

    seem suddenly hv some bug

--- src/components/Ａｂｏｕｔ/About.js
+++ src/components/Ａｂｏｕｔ/About.js
@@ -3,13 +3,29 @@ import React from 'react';
 function About() {
     return (
         <div>
-            <h1>wow</h1>
-            <div>a</div>
-            <div>b</div>
-            <div>c</div>
+
+            <h1>About Us</h1>
+            <div>The most versatile typing website you'll ever see.
+                <br/>Here, you can train your typing skills in all areas including:
+                <br/>• English typing
+                <br/>• Number typing
+                <br/>• Code typing
+                <br/>• WebScrap typing (insert a link which contains text e.g. News)
+                <br/>• No Quote typing (type what you want!)
+
+                
+
+            </div>
+
+
+
+
+
+
+
+
+
         </div>
-
-
     )
 }
 
